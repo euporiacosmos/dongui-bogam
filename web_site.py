b19065ec3857c9c33fed7f23124833d668d05bd5
heatstroke-prventer <euporiacosmos@gmail.com>
2024-07-12 13:13:42 +0900

    replace SQlite column name id->num & add ipv4 to integer feature & set off debug mod

--- web_site.py
+++ web_site.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from sentence_transformers import SentenceTransformer
 import numpy as np
-import json
-import sqlite3
-import datetime
+import datetime, json, socket, sqlite3, struct
 
 app = Flask(__name__)
 embedder = SentenceTransformer("jhgan/ko-sroberta-multitask")
@@ -13,6 +11,14 @@ with open("dongui-bogam.json", "r", encoding="utf-8") as file:
 # https://wikidocs.net/24603
 def cos_sim(A, B):
     return np.dot(A, B) / (np.linalg.norm(A) * np.linalg.norm(B))
+
+# https://stackoverflow.com/a/9591005
+def ip2long(ip):
+    """
+    Convert an IP string to long
+    """
+    packedIP = socket.inet_aton(ip)
+    return struct.unpack("!L", packedIP)[0]
 
 @app.route('/')
 def home():
@@ -38,12 +44,12 @@ def response():
     with sqlite3.connect('./database/database.sqlite3') as conn:
         curs=conn.cursor()
         if page == 1:
-            curs.execute('INSERT INTO visitor(ipv4, datetime, query, page) VALUES (?, ?, ?, ?)', (client_ip, accessed_time, query, page))
+            curs.execute('INSERT INTO visitor(ipv4, datetime, query, page) VALUES (?, ?, ?, ?)', (ip2long(client_ip), accessed_time, query, page))
         else:
-            curs.execute('UPDATE visitor SET page=? WHERE ipv4=? AND datetime=?', (page, client_ip, accessed_time))
+            curs.execute('UPDATE visitor SET page=? WHERE ipv4=? AND datetime=?', (page, ip2long(client_ip), accessed_time))
         conn.commit()
     conn.close()
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='127.0.0.1')
